9b02471dd20452cc15c45676dbfe25e9444360fc
John Joyce <jjoyce88>
2017-12-21 20:50:21 -0500

    Added Firebase scripts and fireBaseUsers.js

---
+++ js/firebaseUsers.js
@@ -0,0 +1,43 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyAxBKyA_lhLfSxekoM6VRMciqin1BWPf78",
+authDomain: "cinematixdatabase.firebaseapp.com",
+databaseURL: "https://cinematixdatabase.firebaseio.com",
+projectId: "cinematixdatabase",
+storageBucket: "",
+messagingSenderId: "500516738721"
+};
+firebase.initializeApp(config);
+
+
+
+
+/* User Login & Signup Stuff
+
+// Call to Create User with Email and Password
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+// Call to Sign In with Email and Password
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+// Call to Sign Out
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+*/
+
+/* 
+
+*/
