78e36018cdb5ad25a0caf353c582061b95d88150
Steve Harold <stehar14@yahoo.com>
2018-01-04 02:09:31 -0500

    implemented pagination on discover page, reorganized JS file

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
 // CinemaTix Site
 // Project 1
 
-//hide discover results div (on discover page) on page load
-$(document).ready(function() {
-  $('#discover-results').hide();
-});
-// On click event listener for #searchButton on searchPage.html 
-$("#searchButton").on("click", function() {
-  // Prevents page reload
-  event.preventDefault();
-  // Makes sure that the text box is populated when user clicks submit
+// Global variable to store page value for query urls
+var page;
+
+var res;
+// Global search function
+function search() {
   if ($("#searchPhrase").val().trim() === "") {
     alert("That's not a valid input!");
   } else{
   // Show discover results div
     $("#discover-results").show();
+    $("#search-change").show();
+    $("#discover-change").hide();
   // Removes any previous movie entries from the #movies div   
     $("#movies").empty();
   // Creates a query URL including user search phrase  
     var phrase = $("#searchPhrase").val().trim();
-    var movieURL= "https://api.themoviedb.org/3/search/movie?api_key=ac004416c837056eac779513d15becfb&language=en-US&query=" + phrase + "&page=1&include_adult=false"
+    var movieURL= "https://api.themoviedb.org/3/search/movie?api_key=ac004416c837056eac779513d15becfb&language=en-US&query=" + phrase + "&page=" + page + "&include_adult=false"
   // AJAX call to get data from TMDB using query URL  
     $.ajax({
       url: movieURL,
       method: "GET"
     }).done(function(response) {
-    // For loop to create 10 collapsible elements and append them to them to the #movies div 
-        for (i=0; i<10; i++){
+      res = response;
+    // For loop to create 20 collapsible elements and append them to them to the #movies div 
+        for (i=0; i<20; i++){
           console.log(response.results[i]);
           // Creating variables to hold response data
           var title = response.results[i].title;
@@ -46,6 +46,147 @@ $("#searchButton").on("click", function() {
           Materialize.toast('test', 2000);
         });
     });
+  }
+}
+// Global discover function
+function discover() {
+  $('#discover-results').show();
+  $("#search-change").hide();
+  $("#discover-change").show();
+  // Removes any previous movie entries from the #movies list   
+  $("#movies").empty();
+// Creates variables to hold user inputs from discover search 
+  var actor = $("#actor").val().trim();
+  if (actor === "") {  
+    var sort = $("#sort").val();
+    var genre = $("#genre").val();
+    var years = snapSlider.noUiSlider.get();
+    var lowYear = years[0];
+    var highYear = years[1];
+    console.log(actor);
+    console.log(sort);
+ 
+    console.log(genre);
+    console.log(lowYear);
+    console.log(highYear);
+// Creates query URL based on user inputs
+    var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&page=" + page + "&include_adult=false"
+// AJAX call to get data from TMDB using query URL  
+    $.ajax({
+      url: movieURL,
+      method: "GET"
+    }).done(function(response) {
+      res=response;
+      console.log(response);
+      console.log(movieURL);
+  // For loop to create 20 collapsible elements and append them to them to the #movies div 
+      for (i=0; i<20; i++){
+        console.log(response.results[i]);
+      // Creating variables to hold response data
+        var title = response.results[i].title;
+        var released = response.results[i].release_date;
+        var plot = response.results[i].overview;
+      // Retrieving the URL for the image
+        var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
+      // Creating a list item for collapsible list #movies
+        var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebaseId='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><a href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'>See if tix are available nearby!</a><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p></div></li>" );
+
+      // Putting the moiveListItem below the previous movies
+        $("#movies").append(movieListItem);
+      }
+    // If you click on the add button on a header, don't show/hide collapsible body
+      $('.add-button').on('click', function(e) {
+        e.stopPropagation();
+        Materialize.toast('test', 2000);
+      });
+    });
+  } else {
+    var actorURL = "https://api.themoviedb.org/3/search/person?api_key=ac004416c837056eac779513d15becfb&query=" + actor;
+    $.ajax({
+      url: actorURL,
+      method: "GET"
+    }).done(function(response) {
+      res=response;
+      var actorId = response.results[0].id;  
+  
+      var sort = $("#sort").val();
+    
+      var genre = $("#genre").val();
+      var years = snapSlider.noUiSlider.get();
+      var lowYear = years[0];
+      var highYear = years[1];
+      console.log(actor);
+      console.log(sort);
+   
+      console.log(genre);
+      console.log(lowYear);
+      console.log(highYear);
+  // Creates query URL based on user inputs
+      var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&with_cast=" + actorId + "&page=" + page + "&include_adult=false"
+  // AJAX call to get data from TMDB using query URL  
+      $.ajax({
+        url: movieURL,
+        method: "GET"
+      }).done(function(response) {
+        res=response;
+        console.log(response);
+        console.log(movieURL);
+    // For loop to create 20 collapsible elements and append them to them to the #movies div 
+        for (i=0; i<20; i++){
+          console.log(response.results[i]);
+        // Creating variables to hold response data
+          var title = response.results[i].title;
+          var released = response.results[i].release_date;
+          var plot = response.results[i].overview;
+        // Retrieving the URL for the image
+          var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
+        // Creating a list item for collapsible list #movies
+          var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p></div></li>" );
+
+        // Putting the moiveListItem below the previous movies
+          $("#movies").append(movieListItem);
+        }
+      // If you click on the add button on a header, don't show/hide collapsible body
+        $('.add-button').on('click', function(e) {
+          e.stopPropagation();
+          Materialize.toast('test', 2000);
+        });
+      });
+    });
+  }
+}
+
+//hide discover results div (on discover page) on page load
+$(document).ready(function() {
+  $('#discover-results').hide();
+});
+
+// On click event listener for #searchButton on searchPage.html 
+$("#searchButton").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  page=1;
+  // Makes sure that the text box is populated when user clicks submit
+  search();
+});
+
+$("#search_btn_prev").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page > 1) {
+    page=page-1;
+    // Makes sure that the text box is populated when user clicks submit
+    search();
+  }
+});
+
+$("#search_btn_next").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page < res.total_pages) {
+    page=page+1;
+  // Makes sure that the text box is populated when user clicks submit
+    search();
   }
 });
 
@@ -71,103 +212,26 @@ $("#movies").on("click", ".collapsible-header", function(){
 $("#discover-submit").on("click", function() {
   // Prevents page reload
   event.preventDefault();
-  $('#discover-results').show();
-  // Removes any previous movie entries from the #movies list   
-  $("#movies").empty();
-// Creates variables to hold user inputs from discover search 
-  var actor = $("#actor").val().trim();
-  if (actor === "") {  
-    var sort = $("#sort").val();
-    var genre = $("#genre").val();
-    var years = snapSlider.noUiSlider.get();
-    var lowYear = years[0];
-    var highYear = years[1];
-    console.log(actor);
-    console.log(sort);
- 
-    console.log(genre);
-    console.log(lowYear);
-    console.log(highYear);
-// Creates query URL based on user inputs
-    var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&page=1&include_adult=false"
-// AJAX call to get data from TMDB using query URL  
-    $.ajax({
-      url: movieURL,
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-      console.log(movieURL);
-  // For loop to create 10 collapsible elements and append them to them to the #movies div 
-      for (i=0; i<10; i++){
-        console.log(response.results[i]);
-      // Creating variables to hold response data
-        var title = response.results[i].title;
-        var released = response.results[i].release_date;
-        var plot = response.results[i].overview;
-      // Retrieving the URL for the image
-        var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
-      // Creating a list item for collapsible list #movies
-        var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p></div></li>" );
+  page=1;
+  discover();
+});
 
-      // Putting the moiveListItem below the previous movies
-        $("#movies").append(movieListItem);
-      }
-    // If you click on the add button on a header, don't show/hide collapsible body
-      $('.add-button').on('click', function(e) {
-        e.stopPropagation();
-        Materialize.toast('test', 2000);
-      });
-    });
-  } else {
-    var actorURL = "https://api.themoviedb.org/3/search/person?api_key=ac004416c837056eac779513d15becfb&query=" + actor;
-    $.ajax({
-      url: actorURL,
-      method: "GET"
-    }).done(function(response) {
-      var actorId = response.results[0].id;  
-  
-      var sort = $("#sort").val();
-    
-      var genre = $("#genre").val();
-      var years = snapSlider.noUiSlider.get();
-      var lowYear = years[0];
-      var highYear = years[1];
-      console.log(actor);
-      console.log(sort);
-   
-      console.log(genre);
-      console.log(lowYear);
-      console.log(highYear);
-  // Creates query URL based on user inputs
-      var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&with_cast=" + actorId + "&page=1&include_adult=false"
-  // AJAX call to get data from TMDB using query URL  
-      $.ajax({
-        url: movieURL,
-        method: "GET"
-      }).done(function(response) {
-        console.log(response);
-        console.log(movieURL);
-    // For loop to create 10 collapsible elements and append them to them to the #movies div 
-        for (i=0; i<10; i++){
-          console.log(response.results[i]);
-        // Creating variables to hold response data
-          var title = response.results[i].title;
-          var released = response.results[i].release_date;
-          var plot = response.results[i].overview;
-        // Retrieving the URL for the image
-          var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
-        // Creating a list item for collapsible list #movies
-          var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p></div></li>" );
+$("#discover_btn_prev").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page > 1) {
+    page=page-1;
+    // Makes sure that the text box is populated when user clicks submit
+    discover();
+  }
+});
 
-        // Putting the moiveListItem below the previous movies
-          $("#movies").append(movieListItem);
-        }
-      // If you click on the add button on a header, don't show/hide collapsible body
-        $('.add-button').on('click', function(e) {
-          e.stopPropagation();
-          Materialize.toast('test', 2000);
-        });
-      });
-    });
+$("#discover_btn_next").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page < res.total_pages) {
+    page=page+1;
+  // Makes sure that the text box is populated when user clicks submit
+    discover();
   }
 });
