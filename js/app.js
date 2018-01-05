071b3233be7fdee8cae389041fd0fa85f9c01b3c
Steve Harold <stehar14@yahoo.com>
2018-01-05 18:48:49 -0500

    Thoroughly commented all files, made variable/id/class names consistent, delete buttons now function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,19 @@
 
 // Global variable to store page value for query urls
 var page;
+
 // Global variable to hold response from ajax 
 var res;
+
+//hide discover results div (on discover page) on page load
+$(document).ready(function() {
+  $('#discover-results').hide();
+});
+
 // Global search function
 function search() {
-  if ($("#searchPhrase").val().trim() === "") {
+// If-else to make sure search-phrase isn't blank  
+  if ($("#search-phrase").val().trim() === "") {
     $("#modal1").show();
   } else{
   // Update page text, show discover results div, show search next/prev, hide discover next/prev
@@ -18,7 +26,7 @@ function search() {
   // Removes any previous movie entries from the #movies div   
     $("#movies").empty();
   // Creates a query URL including user search phrase  
-    var phrase = $("#searchPhrase").val().trim();
+    var phrase = $("#search-phrase").val().trim();
     var movieURL= "https://api.themoviedb.org/3/search/movie?api_key=ac004416c837056eac779513d15becfb&language=en-US&query=" + phrase + "&page=" + page + "&include_adult=false"
   // AJAX call to get data from TMDB using query URL  
     $.ajax({
@@ -26,40 +34,42 @@ function search() {
       method: "GET"
     }).done(function(response) {
       res = response;
-    // For loop to create 20 collapsible elements and append them to them to the #movies div 
-        for (i=0; i<20; i++){
-          console.log(response.results[i]);
-          // Creating variables to hold response data
-          var title = response.results[i].title;
-          var released = response.results[i].release_date;
-          var plot = response.results[i].overview;
-          // Retrieving the URL for the image
-          var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
-          // Creating a list item for collapsible list #movies
-          var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebaseId='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
-
-          // Putting the moiveListItem below the previous movies
-          $("#movies").append(movieListItem);
-        }
-        // If you click on the add button on a header, don't show/hide collapsible body
-        $('.add-button').on('click', function(e) {
-          e.stopPropagation();
-          Materialize.toast('Added!', 2000);
-          var movieNum = $(this).attr("firebaseId");
-          addMovie(movieNum);
-          console.log(movieNum);
-        });
+    // For loop to create collapsible elements (max 20 if the results page is full) and append to the #movies ul
+      for (i=0; i<response.results.length; i++){
+        console.log(response.results[i]);
+      // Creating variables to hold response data
+        var title = response.results[i].title;
+        var released = response.results[i].release_date;
+        var plot = response.results[i].overview;
+      // Retrieving the URL for the image
+        var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
+      // Creating a list item for collapsible list #movies
+        var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebase-id='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
+      // Putting the moiveListItem below the previous movies
+        $("#movies").append(movieListItem);
+      }
+    
+    // If you click on the add button on a header, don't show/hide collapsible body
+    // Instead, add the firebase-id (holding the tmdb movie id) to realtime database
+      $('.add-button').on('click', function(e) {
+        e.stopPropagation();
+        Materialize.toast('Added!', 2000);
+        var movieNum = $(this).attr("firebase-id");
+        addMovie(movieNum);
+        console.log(movieNum);
+      });
     });
   }
 }
+
 // Global discover function
 function discover() {
-  // Update page text, show discover results div, show discover next/prev, hide search next/prev
+// Update page text, show discover results div, show discover next/prev, hide search next/prev
   $(".page").text(page);
   $('#discover-results').show();
   $("#search-change").hide();
   $("#discover-change").show();
-  // Removes any previous movie entries from the #movies list   
+// Removes any previous movie entries from the #movies list   
   $("#movies").empty();
 // Creates variable to hold user input for actor
   var actor = $("#actor").val().trim();
@@ -72,13 +82,12 @@ function discover() {
     var highYear = years[1];
     console.log(actor);
     console.log(sort);
- 
     console.log(genre);
     console.log(lowYear);
     console.log(highYear);
-// Creates query URL based on user inputs
+  // Creates query URL based on user inputs
     var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&page=" + page + "&include_adult=false"
-// AJAX call to get data from TMDB using query URL  
+  // AJAX call to get data from TMDB using query URL  
     $.ajax({
       url: movieURL,
       method: "GET"
@@ -86,8 +95,8 @@ function discover() {
       res=response;
       console.log(response);
       console.log(movieURL);
-  // For loop to create 20 collapsible elements and append them to them to the #movies div 
-      for (i=0; i<20; i++){
+    // For loop to create collapsible elements (max 20 if the results page is full) and append to the #movies ul
+      for (i=0; i<response.results.length; i++){
         console.log(response.results[i]);
       // Creating variables to hold response data
         var title = response.results[i].title;
@@ -96,22 +105,23 @@ function discover() {
       // Retrieving the URL for the image
         var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
       // Creating a list item for collapsible list #movies
-        var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebaseId='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
+        var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebase-id='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
 
       // Putting the moiveListItem below the previous movies
         $("#movies").append(movieListItem);
       }
     // If you click on the add button on a header, don't show/hide collapsible body
+    // Instead, add the firebase-id (holding the tmdb movie id) to realtime database
       $('.add-button').on('click', function(e) {
         e.stopPropagation();
         Materialize.toast('Added!', 2000);
-        var movieNum = $(this).attr("firebaseId");
+        var movieNum = $(this).attr("firebase-id");
         addMovie(movieNum);
         console.log(movieNum);
       });
     });
   } else {
-// Else there is actor input
+  // Else there is actor input
     var actorURL = "https://api.themoviedb.org/3/search/person?api_key=ac004416c837056eac779513d15becfb&query=" + actor;
     $.ajax({
       url: actorURL,
@@ -119,22 +129,19 @@ function discover() {
     }).done(function(response) {
       res=response;
       var actorId = response.results[0].id;  
-  
       var sort = $("#sort").val();
-    
       var genre = $("#genre").val();
       var years = snapSlider.noUiSlider.get();
       var lowYear = years[0];
       var highYear = years[1];
       console.log(actor);
       console.log(sort);
-   
       console.log(genre);
       console.log(lowYear);
       console.log(highYear);
-  // Creates query URL based on user inputs
+    // Creates query URL based on user inputs
       var movieURL= "https://api.themoviedb.org/3/discover/movie?api_key=ac004416c837056eac779513d15becfb&original_language=en-US&sort_by=" + sort + "&release_date.gte=" + lowYear + "&release_date.lte=" + highYear + "&with_genres=" + genre + "&with_cast=" + actorId + "&page=" + page + "&include_adult=false"
-  // AJAX call to get data from TMDB using query URL  
+    // AJAX call to get data from TMDB using query URL  
       $.ajax({
         url: movieURL,
         method: "GET"
@@ -142,8 +149,8 @@ function discover() {
         res=response;
         console.log(response);
         console.log(movieURL);
-    // For loop to create 20 collapsible elements and append them to them to the #movies div 
-        for (i=0; i<20; i++){
+    // For loop to create collapsible elements (max 20 if the results page is full) and append to the #movies ul
+        for (i=0; i<response.results.length; i++) {  
           console.log(response.results[i]);
         // Creating variables to hold response data
           var title = response.results[i].title;
@@ -152,16 +159,16 @@ function discover() {
         // Retrieving the URL for the image
           var imgURL = "https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path;
         // Creating a list item for collapsible list #movies
-          var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
-
+          var movieListItem = $("<li> <div class='collapsible-header movie center' tmdb-id='" + response.results[i].id + "' youtube-search='" + response.results[i].title + " official trailer'><span class='accordion-head-text'>" + response.results[i].title + "</span><a firebase-id='" + response.results[i].id + "' class='add-button btn-floating btn-large waves-effect waves-light red'><i class='material-icons center'>add</i></a></div><div class= 'collapsible-body'><img class='poster col s3 m3 l3' src='https://image.tmdb.org/t/p/w185/" + response.results[i].poster_path + "'><p>" + response.results[i].overview + "</p> <p>Release date: " + response.results[i].release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.results[i].title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>See if this movie is playing near you!</a></div></li>" );
         // Putting the moiveListItem below the previous movies
           $("#movies").append(movieListItem);
         }
       // If you click on the add button on a header, don't show/hide collapsible body
+      // Instead, add the firebase-id (holding the tmdb movie id) to realtime database
         $('.add-button').on('click', function(e) {
           e.stopPropagation();
           Materialize.toast('Added!', 2000);
-          var movieNum = $(this).attr("firebaseId");
+          var movieNum = $(this).attr("firebase-id");
           addMovie(movieNum);
           console.log(movieNum);
         });
@@ -170,30 +177,26 @@ function discover() {
   }
 }
 
-//hide discover results div (on discover page) on page load
-$(document).ready(function() {
-  $('#discover-results').hide();
-});
-
-// On click event listener for #searchButton on searchPage.html 
-$("#searchButton").on("click", function() {
+// On click event listener for #search-button
+$("#search-button").on("click", function() {
   // Prevents page reload
   event.preventDefault();
   page=1;
   search();
 });
 
-$("#search_btn_prev").on("click", function() {
+// On click event listener for previous button on search queries
+$("#search-btn-prev").on("click", function() {
   // Prevents page reload
   event.preventDefault();
   if (page > 1) {
     page=page-1;
-
     search();
   }
 });
 
-$("#search_btn_next").on("click", function() {
+// On click event listener for next button on search queries
+$("#search-btn-next").on("click", function() {
   // Prevents page reload
   event.preventDefault();
   if (page < res.total_pages) {
@@ -202,7 +205,35 @@ $("#search_btn_next").on("click", function() {
   }
 });
 
-// On click event listener for dynamically created .collapsible-headers in the #movies div
+// On click event listener for #discover-submit 
+$("#discover-submit").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  page=1;
+  discover();
+});
+
+// On click event listener for previous button on discover queries
+$("#discover-btn-prev").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page > 1) {
+    page=page-1;
+    discover();
+  }
+});
+
+// On click event listener for next button on discover queries
+$("#discover-btn-next").on("click", function() {
+  // Prevents page reload
+  event.preventDefault();
+  if (page < res.total_pages) {
+    page=page+1;
+    discover();
+  }
+});
+
+// On click event listener for dynamically created .collapsible-headers in the #movies ul
 $("#movies").on("click", ".collapsible-header", function(){
   // Removes any previously loaded youtube videos
   $(".dynamic-iframe").remove();
@@ -220,43 +251,27 @@ $("#movies").on("click", ".collapsible-header", function(){
   });
 });
 
-// On click event listener for #discover-submit button on discoverPage.html
-$("#discover-submit").on("click", function() {
-  // Prevents page reload
-  event.preventDefault();
-  page=1;
-  discover();
-});
-
-$("#discover_btn_prev").on("click", function() {
-  // Prevents page reload
-  event.preventDefault();
-  if (page > 1) {
-    page=page-1;
-
-    discover();
-  }
-});
-
-$("#discover_btn_next").on("click", function() {
-  // Prevents page reload
-  event.preventDefault();
-  if (page < res.total_pages) {
-    page=page+1;
-    discover();
-  }
-});
-
+// On click event listener for close button on modal pop ups
 $("#modal-close").on("click", function () {
   $("#modal1").hide();
   $("#modal2").hide();
 });
 
+// On click event listener for #my-movies collapsible-header on profile page
+// (getMovieList() function located in userData.js)
 $("#my-movies").on("click", function() {
+  $('#profile-info').empty();
   getMovieList();
 });
 
-$('#profileinfo').on('click', ".delete", function(e) {
+// On click event listener for delete buttons on profile page
+$('#profile-info').on('click', ".delete", function(e) {
   e.stopPropagation();
-  Materialize.toast('test', 2000);
+  Materialize.toast('Removed!', 2000);
+  var firebaseMovieNum = $(this).attr("firebase-id");
+  deleteMovie(firebaseMovieNum);
+  console.log(firebaseMovieNum);
+  setTimeout(function() {
+    $('#profile-info').empty();
+    getMovieList()} , 0);
 });
--- js/userData.js
+++ js/userData.js
@@ -1,3 +1,6 @@
+// CinemaTix Site
+// Project 1
+
 // Initialize Firebase
 var config =
 {
@@ -10,16 +13,16 @@ var config =
 };
 firebase.initializeApp(config);
 
+// Global variables to hold reference to firebase database and user id
 var database = firebase.database();
 var userId;
 
-//Register button
+// On click function for the register button on reg page
 $("#register").on("click", e =>
 {
   e.preventDefault();
   e.stopPropagation();
-
-  // Make reference variables for the email and password entered on the page
+// Make reference variables for the display name, email, and password entered on the page
   const txtEmail = document.getElementById("email");
   const txtPassword = document.getElementById("password");
   const txtDisplay = document.getElementById("display-name");
@@ -27,18 +30,19 @@ $("#register").on("click", e =>
   const email = txtEmail.value;
   const password = txtPassword.value;
   const auth = firebase.auth();
-
   console.log("Email: " + email);
   console.log("Password: " + password);
-
+// Create promise to create new user using email and password
   const promise = auth.createUserWithEmailAndPassword(email, password);
   promise
+  // Display error in modal    
     .catch(e => 
       {
         console.log(e.message)
         $("#error-msg").text(e.message);
         $("#modal2").show();
       })
+  // Then send email to verify the users email address
     .then(e =>
   {
     var curser = auth.currentUser;
@@ -53,51 +57,53 @@ $("#register").on("click", e =>
       $("#error-msg").text(e.message);
       $("#modal2").show();
     });
-
+  // Create uid for user set to display value 
     createUserData(curser.uid, display);
 
    });
 });
 
-// Sign-in Button
+// On click event listener for the Sign-in Button on reg page
 $("#sign-in").on("click", e =>
 {
+  // Don't reload the page
   e.preventDefault();
   e.stopPropagation();
-
   // Make reference variables for the email and password entered on the page
   const txtEmail = document.getElementById("email");
   const txtPassword = document.getElementById("password");
-
   const email = txtEmail.value;
   const password = txtPassword.value;
   const auth = firebase.auth();
-
   console.log("Email: " + email);
   console.log("Password: " + password);
-
+// Create promise to sign user in using the email and password entered
   const promise = auth.signInWithEmailAndPassword(email, password);
   promise
   .catch(e => 
    {
+  // Display error in modal
      console.log(e.message);
      $("#error-msg").text(e.message);
       $("#modal2").show();
    })
+  // After successful sign in, redirect to profile page
    .then(e =>
     {
       window.location.href="profile.html";
     });
 });
 
-// Sign-out button
+// On click event listener for Sign-out button on reg page
 $("#sign-out").on("click", e =>
 {
     firebase.auth().signOut();
 });
+
 // Realtime listener
 firebase.auth().onAuthStateChanged(firebaseUser =>
 {
+// If a user is logged in, hide register and sign in buttons, display sign out
   if(firebaseUser)
   {
     console.log(firebaseUser);
@@ -106,7 +112,7 @@ firebase.auth().onAuthStateChanged(firebaseUser =>
     $("#sign-out").show();
 
     userId = firebase.auth().currentUser.uid;
-
+  //If the signed in user verified their email, allow them to access their profile features
     if (firebaseUser.emailVerified)
   {
     console.log("email verified");
@@ -116,12 +122,14 @@ firebase.auth().onAuthStateChanged(firebaseUser =>
   }
   else
   {
+  // Otherwise show modal indicating their email needs to be verified
     $("#error-msg").text("Please verify your email address by clicking the link in the email we sent you!");
     $("#modal2").show();
     console.log('Email is not verified');
   }
   
   }
+// Otherwise, show register and sign in button, and hide sign out button  
   else
   {
     console.log("Not logged in.");
@@ -131,8 +139,10 @@ firebase.auth().onAuthStateChanged(firebaseUser =>
   }
 });
 
+//Variable to hold database reference to users subset
 var userRef = database.ref("users/");
 
+// Function to initialize user data upon registration
 function createUserData(id, username)
 {
 	userRef.child(id).set(
@@ -143,6 +153,7 @@ function createUserData(id, username)
 	});
 }
 
+// Function to add movie from discover page to your movie list
 function addMovie(movieId, callback)
 {
 	thisUserRef = database.ref("users/" + userId);
@@ -161,39 +172,52 @@ function addMovie(movieId, callback)
   });
 }
 
+function deleteMovie(movieId) {
+  thisUserRef = database.ref("users/" + userId);
+  var moviesRef = thisUserRef.child("movies");
+  moviesRef.orderByChild('id').equalTo(movieId)
+    .once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+        //remove each child
+        moviesRef.child(childSnapshot.key).remove();
+    });
+});
+}
+
+// Function to populate My Movies list on Profile page
 function getMovieList()
 {
+// Set reference point in database  
   var userId = firebase.auth().currentUser.uid;
   var rootRef = database.ref();
   var urlRef = rootRef.child("users/" + userId + "/movies");
   urlRef.on("child_added", function(snapshot) {
-    
-      cardURL= "https://api.themoviedb.org/3/movie/" + snapshot.val().id + "?api_key=ac004416c837056eac779513d15becfb&language=en-US";
-
-      $.ajax({
-        url: cardURL,
-        method: "GET"
-      }).done(function(response) {
-        res=response;
-        console.log(response);
-        console.log(cardURL);
-      // Creating variables to hold response data
-        var title = response.title;
-        var released = response.release_date;
-        var plot = response.overview;
-      // Retrieving the URL for the image
-        var imgURL = "https://image.tmdb.org/t/p/w185/" + response.poster_path;
-      // Creating a list item for collapsible list #movies
-        var movieCard = $("<div class='col s12 m2'><div class='card'><div class='card-image'><img src='" + imgURL + "'><span class='card-title'>" + title + "</span><a class='delete btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>cancel</i></a></div><div class='card-content black'><p>Release: " + response.release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>Tickets?</a></div></div></div>");
-
-        // Putting the moiveListItem below the previous movies
-        $("#profileinfo").append(movieCard);
-      // If you click on the add button on a header, don't show/hide collapsible body
-    
-      });  
-    });
+  // Creates query URL using the movie ids stored in the realtime database for the current user
+  // Runs an ajax call to obtain movie info from TMDB and creates a Materialize card for each movie
+  // and appends it to the profile-info span in the collapsible-body on the profile page
+    cardURL= "https://api.themoviedb.org/3/movie/" + snapshot.val().id + "?api_key=ac004416c837056eac779513d15becfb&language=en-US";
+    $.ajax({
+      url: cardURL,
+      method: "GET"
+    }).done(function(response) {
+      res=response;
+      console.log(response);
+      console.log(cardURL);
+    // Creating variables to hold response data
+      var title = response.title;
+      var released = response.release_date;
+      var plot = response.overview;
+    // Retrieving the URL for the image
+      var imgURL = "https://image.tmdb.org/t/p/w185/" + response.poster_path;
+    // Creating a list item for collapsible list #movies
+      var movieCard = $("<div class='col s12 m2'><div class='card'><div class='card-image'><img src='" + imgURL + "'><span class='card-title'>" + title + "</span><a firebase-id='" + response.id + "' class='delete btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>cancel</i></a></div><div class='card-content black'><p>Release: " + response.release_date + "</p><a class='ticket-link' href ='https://www.atomtickets.com/search?query=" + response.title + "'><img src='./assets/images/atomtix.jpg' height=40px width=40px>Tickets?</a></div></div></div>");
+    // Putting the moiveCard behind the previous movies
+      $("#profile-info").append(movieCard);
+    });  
+  });
 }
 
+// Function to display join date on profile page
 function getJoinDate()
 {
 	console.log(userId);
@@ -201,23 +225,25 @@ function getJoinDate()
 	thisUserRef.once("value", function(snapshot)
 	{
 		console.log(snapshot.val().joinDate);
-		$("#datejoined").html(snapshot.val().joinDate);
+		$("#date-joined").html("Member since: " + snapshot.val().joinDate);
   });
 }
 
+// Function to display username on profile page
 function getUsername()
 {
 	thisUserRef = database.ref("users/" + userId);
   thisUserRef.once("value", function(snapshot)
   {
     console.log(snapshot.val().username);
-    $("#usernamedisplay").html(snapshot.val().username);
+    $("#username-display").html(snapshot.val().username);
   });
 }
 
+// Function to display email on profile page
 function getEmail()
 {
 	var email = firebase.auth().currentUser.email;
 	console.log(email);
-	$("#emailused").html(email);
+	$("#email-used").html(email);
 }
